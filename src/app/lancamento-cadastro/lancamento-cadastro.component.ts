48853bb21ab869bfe49c57ddd0a7598a23f69069
alexisbrabo <alexis_brabo@hotmail.com>
2018-10-29 18:11:01 -0300

    Aula 13.4

--- src/app/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -15,6 +15,15 @@ export class LancamentoCadastroComponent implements OnInit {
     { label: 'Receita', value: 'RECEITA' },
     { label: 'Despesa', value: 'DESPESA' }
   ];
+  categorias = [
+    { label: 'Alimentação', value: 1 },
+    { label: 'Transporte', value: 2 }
+  ];
+  pessoas = [
+    { label: 'João da Silva', value: 1 },
+    { label: 'Sebastião Souza', value: 2 },
+    { label: 'MAria Augusta', value: 3 }
+  ];
 
   constructor(private app: AppComponent) { }
 
