f0cfe9f353ef866a8c6257a44a94972db1369b27
alexisbrabo <alexis_brabo@hotmail.com>
2018-11-29 13:05:50 -0300

    Aula 19.14

--- src/app/core/error-handler.service.ts
+++ src/app/core/error-handler.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
-import { MessageService } from 'primeng/components/common/api';
+import { Router } from '@angular/router';
+import { NotAuthenticatedError } from '../seguranca/money-http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorHandlerService {
 
-  constructor(private messageService: MessageService) {
+  constructor(private router: Router) {
 
   }
 
@@ -15,17 +16,19 @@ export class ErrorHandlerService {
     let msg: string;
     if (typeof error === 'string') {
       msg = error;
+    } else if (error instanceof NotAuthenticatedError) {
+      msg = 'Sua sessão expirou!';
+      this.router.navigate(['/login']);
+    } else if (error.status === 400 && error.error && error.error.error === 'invalid_grant') {
+      msg = 'Usuário ou Senha inválida!';
+    } else if (error.status === 403) {
+      msg = 'Você não tem permissão para esta ação!';
+    } else if (error.status.toString().startsWith(4)) {
+      msg = error.error[0].mensagemUsuario;
     } else {
-      if (error.status === 400 && error.error && error.error.error === 'invalid_grant') {
-        msg = 'Usuário ou Senha inválida!';
-      } else if (error.status === 403) {
-        msg = 'Você não tem permissão para esta ação!';
-      } else if (error.status.toString().startsWith(4)) {
-        msg = error.error[0].mensagemUsuario;
-      } else {
-        msg = 'Falha no processamento. Tente Novamente';
-      }
+      msg = 'Falha no processamento. Tente Novamente';
     }
+
     console.log('erro', error);
     return throwError(msg);
   }
