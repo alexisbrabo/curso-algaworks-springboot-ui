5849eca8cdd70ca937002e15419a08696135cebd
alexisbrabo <alexis_brabo@hotmail.com>
2018-12-06 09:31:54 -0300

    Deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://alexismoney-api.herokuapp.com/',
+  apiUrl: 'https://alexismoney-api.herokuapp.com',
   tokenWhitelistedDomains: ['localhost:8080'],
   tokenBlacklistedDomains: ['localhost:8080\/oauth\/token'],
 };
