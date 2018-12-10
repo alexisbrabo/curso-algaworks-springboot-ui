bc497832e5dc86e237dce5186ee9665cf44712f2
alexisbrabo <alexis_brabo@hotmail.com>
2018-12-10 08:03:16 -0300

    Aula 21.6

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,26 +4,20 @@ import { NgModule } from '@angular/core';
 import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
 import { NaoAutorizadoComponent } from './core/nao-autorizado.component';
 
-import { LancamentosRoutingModule } from './lancamentos/lancamentos-routing.module';
-import { PessoasRoutingModule } from './pessoas/pessoas-routing.module';
-import { SegurancaRoutingModule } from './seguranca/seguranca-routing.module';
-
 const routes: Routes = [
-    { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
-    { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
-    { path: 'nao-autorizado', component: NaoAutorizadoComponent },
-    { path: '**', redirectTo: 'pagina-nao-encontrada' }
+  { path: 'lancamentos', loadChildren: './lancamentos/lancamentos.module#LancamentosModule' },
+  { path: 'pessoas', loadChildren: './pessoas/pessoas.module#PessoasModule' },
+
+  { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
+  { path: 'nao-autorizado', component: NaoAutorizadoComponent },
+  { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent },
+  { path: '**', redirectTo: 'pagina-nao-encontrada' }
 ];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(routes),
-        LancamentosRoutingModule,
-        PessoasRoutingModule,
-        SegurancaRoutingModule
-    ],
-    exports: [
-        RouterModule
-    ]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
